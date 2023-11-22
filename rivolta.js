9c8a80bf356dab7b09c91a1f54a8e06c9bbf32a6
MdF <85852542+ER-2021-MDF@users.noreply.github.com>
2023-11-22 18:17:15 +0100

    Update rivolta.js

--- rivolta.js
+++ rivolta.js
@@ -71,7 +71,7 @@ function sbloccaSfida(sfida){
 		// ----- PER LE SFIDE ----- 
 		
 		case "3406966054":
-			title = "CONTROLLA IL PERIMETRO";
+			title = "PIAZZA LA BOMBA";
 			
 			img = "";
 			
@@ -87,7 +87,7 @@ function sbloccaSfida(sfida){
 			indizio = "Indizio: ti verr\u00E0 fornito alla fine della sfida";			
 		break;
 		
-		case "geunagiuseppe":
+		case "besialegermano":
 			title = "IL DISCORSO";
 			
 			img = "";
@@ -99,7 +99,7 @@ function sbloccaSfida(sfida){
 			indizio = "Ti verr\u00E0 fornito alla fine della sfida";
 		break;
 		
-		case "9:0012:00":
+		case "cpia5torino.edu.it":
 			title = "TRIBUNALE";
 			
 			img = "";
@@ -125,7 +125,7 @@ function sbloccaSfida(sfida){
 			indizio = "Indizio: ti verr\u00E0 fornito alla fine della sfida";
 		break;
 		
-		case "gruppogiovanidellaparrocchiamadonnadifatimadipinerolo":
+		case "aibambinichestannocrescendo?":
 			title = "IL PALAZZO";
 			
 			img = "";
@@ -137,7 +137,7 @@ function sbloccaSfida(sfida){
 			indizio = "Indizio: ti verr\u00E0 fornito alla fine della sfida";
 		break;
 			
-		case "8":
+		case "4":
 			title = "LA PIAZZA";
 			
 			img = "";
@@ -174,7 +174,7 @@ function sbloccaSfida(sfida){
 		
 			text = "";
 			
-			indizio = "Numero di colonne dentro al duomo (no paraste)";
+			indizio = "Numero di santi affrescati sulla facciata del duomo";
 			
 			immagine = true;
 		break;
@@ -188,7 +188,7 @@ function sbloccaSfida(sfida){
 		
 			text = "";
 			
-			indizio = "società italiana degli autori ed editori, orario lunedì - venerdì dalle … alle …";
+			indizio = "targa commemorativa, quinto cognome e nome colonna di destra";
 			
 			immagine = true;
 		break;
@@ -216,7 +216,7 @@ function sbloccaSfida(sfida){
 		
 			text = "";
 			
-			indizio = "Sentiero laudato sii tappa 1, realizzata dal ...";
+			indizio = "Sentiero laudato sii: Che tipo di mondo desideriamo trasmettere a coloro che verranno dopo di noi? ...";
 			
 			immagine = true;
 			
@@ -225,22 +225,22 @@ function sbloccaSfida(sfida){
 		// ----- PER IL TUTORIAL ----- 
 		
 		case "start":
-			title = "MY GESTAPO"
+			title = "MY SPACE"
 			
 			img = "";
 			
-			desc = "Un solo regno";
+			desc = "Una sola Terra";
 			
 			text = "Grazie a questa applicazione, la base pu\u00F2 comunicare direttamente con i soldati sul campo. In alto troverai sempre il titolo della missione, "+
-					"(MY GESTAPO per adesso), subito sotto la storia collegata a questa missione (ora \'un solo regno\') e al posto di questo testo troverai sempre "+
+					"(MY SPACE per adesso), subito sotto la storia collegata a questa missione (ora \'una sola Terra\') e al posto di questo testo troverai sempre "+
 					"le indicazioni per portare a termine la missione. Infine sotto hai la possibilit\u00E0 di mettere una parola d'odrine che ti verr\u00E0 fornita "+
 					"tramite suggerimento o come ricompensa e ti servir\u00E0 per avanzare alla missione successiva."
 					
-			indizio = "Per iniziare digita Batty";
+			indizio = "Per iniziare digita Bubi";
 			
 			break;
 			
-		case "batty":
+		case "bubi":
 			alert("PASSWORD ERRATA");
 			
 			title = "ERRORE";
@@ -253,7 +253,7 @@ function sbloccaSfida(sfida){
 					"ma non preoccuparti: basta inserire l'ultima password che hai ottenuto e tornerai al punto di prima. Facciamo un esempio: con la password suggerita "+
 					"qua sotto tornerai alla pagina precedente. Fai attenzione: il tutorial \u00E8 finito, da adesso inizia la missione!";
 			
-			indizio = "Scrivi start e premi OK, poi dopo non scrivere Batty ma inserisci la password che vi \u00E8 stata data sul biglietto";
+			indizio = "Scrivi start e premi OK, poi dopo non scrivere Bubi ma inserisci la password che vi \u00E8 stata data sul biglietto";
 			
 			break;
 			
